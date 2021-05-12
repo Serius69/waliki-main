01290ecb3fce4509bf0f6ae20dcb40f284d30d32
Serius69 <seriogionis@gmail.com>
2021-05-12 09:08:43 -0400

    Modificado de los dao y api

--- src/main/java/bo/edu/ucb/ingsoft/waliki/main/api/UsuarioApi.java
+++ src/main/java/bo/edu/ucb/ingsoft/waliki/main/api/UsuarioApi.java
@@ -1,5 +1,84 @@
 package bo.edu.ucb.ingsoft.waliki.main.api;
 
+import bo.edu.ucb.ingsoft.waliki.main.bl.UsuarioBl;
+import bo.edu.ucb.ingsoft.waliki.main.dto.DonadorDto;
+import bo.edu.ucb.ingsoft.waliki.main.dto.PersonaDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.server.ResponseStatusException;
+
+import javax.sql.DataSource;
+import java.util.List;
+
 public class UsuarioApi {
 
+    @Autowired
+    public DataSource dataSource;
+    @Autowired
+    private UsuarioBl usuarioBl;
+    @Autowired
+    private UsuarioBl gestionPersonaBl;
+
+    @GetMapping(path = "/persona/{personaId}")
+    public PersonaDto findPersonaById(@PathVariable Integer personaId) {
+        PersonaDto persona = gestionPersonaBl.findPersonaById(personaId);
+        if (persona != null) {
+            return gestionPersonaBl.findPersonaById(personaId);
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No existe la persona con codigo:" + personaId );
+        }
+    }
+
+    @GetMapping(path = "/persona")
+    public List<PersonaDto> findAllPersonas() {
+        return gestionPersonaBl.findAllPersonas();
+    }
+
+    @PostMapping(path = "/persona")
+    public PersonaDto createPersona(@RequestBody PersonaDto persona) {
+        // Validar que los datos enviados son correctos.
+        if (persona.nombre == null || persona.nombre.trim().equals("")) {  // nombre: "     "
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "El nombre debe ser obligatorio" );
+        }
+
+        if (persona.apellidoPaterno == null || persona.apellidoPaterno.trim().equals("")) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "El apellido debe ser obligatorio" );
+        }
+
+        return gestionPersonaBl.crearPersona(persona);
+    }
+
+    @GetMapping(path = "/donador/{donadorId}")
+    public DonadorDto findDonadorById(@PathVariable Integer personaId) {
+        DonadorDto donador = usuarioBl.findDonadorById(personaId);
+        if (donador != null) {
+            return usuarioBl.findDonadorById(personaId);
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No existe la persona con codigo:" + personaId );
+        }
+    }
+
+    @GetMapping(path = "/donador")
+    public List<DonadorDto> findAllDonadores() {
+        return usuarioBl.findAllDonadores();
+    }
+
+
+    @PostMapping(path = "/donador")
+    public DonadorDto createDonador(@RequestBody DonadorDto donador) {
+        // Validar que los datos enviados son correctos.
+        if (donador.donadorId == null || donador.contratoId.equals("")) {  // nombre: "     "
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "El nombre debe ser obligatorio" );
+        }
+
+        if (donador.donadorId == null || donador.contratoId.equals("")) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "El apellido debe ser obligatorio" );
+        }
+
+        return usuarioBl.crearDonador(donador);
+    }
 }
--- src/main/java/bo/edu/ucb/ingsoft/waliki/main/bl/UsuarioBl.java
+++ src/main/java/bo/edu/ucb/ingsoft/waliki/main/bl/UsuarioBl.java
@@ -1,13 +1,36 @@
 package bo.edu.ucb.ingsoft.waliki.main.bl;
 
 import bo.edu.ucb.ingsoft.waliki.main.dao.DonadorDao;
+import bo.edu.ucb.ingsoft.waliki.main.dao.PersonaDao;
 import bo.edu.ucb.ingsoft.waliki.main.dao.UsuarioDao;
+import bo.edu.ucb.ingsoft.waliki.main.dto.DonadorDto;
+import bo.edu.ucb.ingsoft.waliki.main.dto.PersonaDto;
 import bo.edu.ucb.ingsoft.waliki.main.dto.UsuarioDto;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
 public class UsuarioBl {
+
+    @Autowired
+    PersonaDao personaDao;
+    DonadorDao donadorDao;
+
+    public PersonaDto crearPersona(PersonaDto persona) {
+        // Computamos el numero de seguro social, conformado por los tres primeros caracteres
+        // del nombre mas los tres primeros del apelliod
+        persona.nroSS = persona.nombre.substring(0,3).toUpperCase() + persona.apellidoPaterno.substring(0,3);
+
+        return personaDao.crearPersona(persona);
+    }
+
+    public PersonaDto findPersonaById(Integer personaId) {
+        return personaDao.findPersonaById(personaId);
+    }
+
+    public List<PersonaDto> findAllPersonas() {
+        return personaDao.findAllPersonas();
+    }
 
     @Autowired
     UsuarioDao usuarioDao;
@@ -20,9 +43,26 @@ public class UsuarioBl {
         return usuarioDao.crearUsuario(usuario);
     }
 
-    public UsuarioDto findDonadorById(Integer usuarioId) {
+    public UsuarioDto findUsuarioById(Integer usuarioId) {
         return usuarioDao.findUsuarioById(usuarioId);
     }
 
+
+
+    public DonadorDto crearDonador(DonadorDto donador) {
+        // Computamos el numero de seguro social, conformado por los tres primeros caracteres
+        // del nombre mas los tres primeros del apelliod
+        //donador.donadorId = donador.nombre.substring(0,3).toUpperCase() + donador.apellido.substring(0,3);
+
+        return donadorDao.crearDonador(donador);
+    }
+
+    public DonadorDto findDonadorById(Integer donadorId) {
+        return donadorDao.findDonadorById(donadorId);
+    }
+
+    public List<DonadorDto> findAllDonadores() {
+        return donadorDao.findAllDonadores();
+    }
 
 }
